9b027bc49522a42368f861fb4c1e5476a349504b
Swizec Teller <swizec@swizec.com>
2019-02-25 14:42:54 -0800

    Fix bad frameborder properties

--- src/widgets/Hero.js
+++ src/widgets/Hero.js
@@ -1,46 +1,56 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import VideoBlock from '../widgets/VideoBlock'
+import React, { Component } from "react";
+import styled from "styled-components";
+import VideoBlock from "../widgets/VideoBlock";
 
 //import Mock1 from '../images/Mock1.png'
 
-
 const Wrapper = styled.div`
-max-width: 700px;
-margin: 0rem auto 0;
-padding: 0 1rem;
-height: 100%;
-text-align: center;
-img {
-    height: 300px;
-    display: block;
-    margin: 0 auto;
-}
-@media (max-width: 940px) {
-  h1 {
-    font-size: 40px;
-  }
-}
+    max-width: 700px;
+    margin: 0rem auto 0;
+    padding: 0 1rem;
+    height: 100%;
+    text-align: center;
+    img {
+        height: 300px;
+        display: block;
+        margin: 0 auto;
+    }
+    @media (max-width: 940px) {
+        h1 {
+            font-size: 40px;
+        }
+    }
 `;
 
-
-const WrapperGroup = styled.div`
-
-`;
+const WrapperGroup = styled.div``;
 
 export default class Hero extends Component {
-  render() {
-    return (
-      <Wrapper>
-        <WrapperGroup>
-            <h1>Stuck Copy/Pasting Random D3 Examples?</h1>
-            <h3>Become a <strong>Data Visualization Engineer</strong> with React for Data Visualization.</h3>
-            <VideoBlock 
-              video={<iframe className='youtubevid' width="560" height="315" title="This is a unique title" src="https://www.youtube.com/embed/7HQzbq4s7MQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}
-              />
-            {/*<img src={Mock1} alt='barchart'/>*/}
-        </WrapperGroup>
-      </Wrapper>
-    )
-  }
+    render() {
+        return (
+            <Wrapper>
+                <WrapperGroup>
+                    <h1>Stuck Copy/Pasting Random D3 Examples?</h1>
+                    <h3>
+                        Become a <strong>Data Visualization Engineer</strong>{" "}
+                        with React for Data Visualization.
+                    </h3>
+                    <VideoBlock
+                        video={
+                            <iframe
+                                className="youtubevid"
+                                width="560"
+                                height="315"
+                                title="This is a unique title"
+                                src="https://www.youtube.com/embed/7HQzbq4s7MQ"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen
+                            />
+                        }
+                    />
+                    {/*<img src={Mock1} alt='barchart'/>*/}
+                </WrapperGroup>
+            </Wrapper>
+        );
+    }
 }
--- src/widgets/Section3.js
+++ src/widgets/Section3.js
@@ -1,81 +1,120 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import styled from "styled-components";
 //import BarChart from '../images/BarChart.png'
-import VideoBlock from '../widgets/VideoBlock'
-
-
-
-
+import VideoBlock from "../widgets/VideoBlock";
 
 const Wrapper = styled.div`
-max-width: 1000px;
-margin: 7rem auto;
-padding: 0 2rem;
-img {
-    height: 300px;
-    display: block;
-    margin: 0 auto;
-}
-h2 {
-  text-align: center;
-}
-p {
-  font-size: 18px;
-}
-.videocase {
-display: grid;
-grid-gap: 4rem;
-grid-template-columns: 1fr 1fr 1fr;
-grid-template-areas:
-'V1 V2 V3';
-.Video1 {
-  grid-area: V1;
-}
-.Video2 {
-  grid-area: V2;
-}
-.Video3 {
-  grid-area: V3;
-}
-@media (max-width: 940px) {
-grid-template-columns: 1fr;
-grid-template-areas:
-'V1'
-'V2'
-'V3';
-  }
-}
+    max-width: 1000px;
+    margin: 7rem auto;
+    padding: 0 2rem;
+    img {
+        height: 300px;
+        display: block;
+        margin: 0 auto;
+    }
+    h2 {
+        text-align: center;
+    }
+    p {
+        font-size: 18px;
+    }
+    .videocase {
+        display: grid;
+        grid-gap: 4rem;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-areas: "V1 V2 V3";
+        .Video1 {
+            grid-area: V1;
+        }
+        .Video2 {
+            grid-area: V2;
+        }
+        .Video3 {
+            grid-area: V3;
+        }
+        @media (max-width: 940px) {
+            grid-template-columns: 1fr;
+            grid-template-areas:
+                "V1"
+                "V2"
+                "V3";
+        }
+    }
 `;
 
 export default class Section1 extends Component {
-  render() {
-    return (
-      <Wrapper>
-        {/*<img src={BarChart} alt='bc' />*/}
-        <h2>Reusable data visualization with React and D3</h2>
-        <div className='videocase'>
-        <div className='Video1'>
-        <VideoBlock 
-        video={<iframe className='youtubevid' title="This is a unique title" width="260" height="315" src="https://www.youtube.com/embed/UP1nCXG2t4M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}
-        />
-        <p>
-        The talk shows a proof of concept approach to making fancy animations with React and d3js - a Space Invaders game. I explain the basic approach, where I got the idea, and show off some code.</p>
-        
-        </div>
-        <div className='Video2'>
-        <VideoBlock
-        video={<iframe className='youtubevid' title="This is a unique title" width="260" height="315" src="https://www.youtube.com/embed/47uMw-2mb4U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}
-        />
-        <p>This talk is more hands on. I show how the animated alphabet, particle generator, and talk about the benefits of componentization when it comes to building modern data visualization.</p>
-        </div>
-        <div className='Video3'>
-        <VideoBlock
-        video={<iframe className='youtubevid' title="This is a unique title" width="260" height="315" src="https://www.youtube.com/embed/9JvIyz7uB2s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}
-        />
-        <p>You don't have to watch the whole talk, it's all in React+D3v4.</p>
-        </div>
-        </div>
-      </Wrapper>
-    )
-  }
+    render() {
+        return (
+            <Wrapper>
+                {/*<img src={BarChart} alt='bc' />*/}
+                <h2>Reusable data visualization with React and D3</h2>
+                <div className="videocase">
+                    <div className="Video1">
+                        <VideoBlock
+                            video={
+                                <iframe
+                                    className="youtubevid"
+                                    title="This is a unique title"
+                                    width="260"
+                                    height="315"
+                                    src="https://www.youtube.com/embed/UP1nCXG2t4M"
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen
+                                />
+                            }
+                        />
+                        <p>
+                            The talk shows a proof of concept approach to making
+                            fancy animations with React and d3js - a Space
+                            Invaders game. I explain the basic approach, where I
+                            got the idea, and show off some code.
+                        </p>
+                    </div>
+                    <div className="Video2">
+                        <VideoBlock
+                            video={
+                                <iframe
+                                    className="youtubevid"
+                                    title="This is a unique title"
+                                    width="260"
+                                    height="315"
+                                    src="https://www.youtube.com/embed/47uMw-2mb4U"
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen
+                                />
+                            }
+                        />
+                        <p>
+                            This talk is more hands on. I show how the animated
+                            alphabet, particle generator, and talk about the
+                            benefits of componentization when it comes to
+                            building modern data visualization.
+                        </p>
+                    </div>
+                    <div className="Video3">
+                        <VideoBlock
+                            video={
+                                <iframe
+                                    className="youtubevid"
+                                    title="This is a unique title"
+                                    width="260"
+                                    height="315"
+                                    src="https://www.youtube.com/embed/9JvIyz7uB2s"
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen
+                                />
+                            }
+                        />
+                        <p>
+                            You don't have to watch the whole talk, it's all in
+                            React+D3v4.
+                        </p>
+                    </div>
+                </div>
+            </Wrapper>
+        );
+    }
 }
