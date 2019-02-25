9540a0d114f4d5328d8d20eb9728c109d4007819
CraftAtoms <creator@craftatoms.com>
2019-02-25 03:05:08 -0500

    imagemeta

--- src/pages/images.js
+++ src/pages/images.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import metaimage from '../images/metaimage.png'
 
 export default class images extends Component {
   render() {
     return (
       <div>
-        
+        <img src={metaimage} alt='meta'/>
       </div>
     )
   }
