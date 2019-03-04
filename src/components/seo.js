27dd39e5eaf35c9a19145a6983d72181fdb9640f
Swizec Teller <swizec@swizec.com>
2019-03-04 11:44:15 -0800

    Add basic thanks page

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
 
-function SEO({ description, lang, meta, keywords, title }) {
+function SEO({ description, lang, meta, keywords, title, includeTypeform }) {
     return (
         <StaticQuery
             query={detailsQuery}
@@ -80,6 +80,9 @@ function SEO({ description, lang, meta, keywords, title }) {
                             defer
                             async
                         />
+                        {includeTypeform ? (
+                            <script>{` (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){js = ce.call(d, "script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() `}</script>
+                        ) : null}
                     </Helmet>
                 );
             }}
