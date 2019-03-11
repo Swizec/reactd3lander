0a5130b61e7fb6db8bbcf13628c05e8029942eb7
Swizec Teller <swizec@swizec.com>
2019-03-10 19:35:14 -0700

    Update FAQ section

--- src/widgets/Faq.js
+++ src/widgets/Faq.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-max-width: 700px;
-margin: 0 auto;
-`
+    max-width: 700px;
+    margin: 0 auto;
+`;
 const WrapperQuestion = styled.div`
-  font-size: 20px;
-  font-weight: 600;
-  margin: 10px 0;
-  color: #384047;
-  padding: 2rem 0 0;
-`
+    font-size: 20px;
+    font-weight: 600;
+    margin: 10px 0;
+    color: #384047;
+    padding: 2rem 0 0;
+`;
 
 const WrapperAnswer = styled.div`
-  font-weight: 100;
-  font-size: 18px;
-  color: #4e616c;
-`
+    &,
+    p {
+        font-weight: 100;
+        font-size: 18px;
+        color: #4e616c;
+    }
+`;
 
 const FAQ = props => (
-  <Wrapper>
-    <WrapperQuestion>{props.question}</WrapperQuestion>
-    <WrapperAnswer>{props.answer}</WrapperAnswer>
-  </Wrapper>
-)
+    <Wrapper>
+        <WrapperQuestion>{props.question}</WrapperQuestion>
+        <WrapperAnswer>{props.answer}</WrapperAnswer>
+    </Wrapper>
+);
 
-export default FAQ
+export default FAQ;
