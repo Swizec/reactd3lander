103c28878cda40cee14f523f1e185461545e2422
Swizec Teller <swizec@swizec.com>
2019-08-25 16:02:50 +0100

    Add a few more visual things

--- src/widgets/Section3.js
+++ src/widgets/Section3.js
@@ -1,72 +1,78 @@
-import React, { Component } from "react";
-import styled from "styled-components";
-import Video from "../widgets/VideoBlock";
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import Video from '../widgets/VideoBlock'
 
-import { Title } from "../styles";
+import { Title } from '../styles'
 
 const Wrapper = styled.div`
-    max-width: 700px;
-    margin: 7rem auto;
-    margin-top: 0;
-    padding: 0 2rem;
+  max-width: 700px;
+  margin: 7rem auto;
+  margin-top: 0;
+  padding: 0 2rem;
 
-    .centered {
-        text-align: center;
-    }
-`;
+  .centered {
+    text-align: center;
+  }
+`
 
 const P = styled.p`
-    max-width: 700px;
-    margin: 2rem auto;
-    padding: 0 2rem;
-`;
+  max-width: 700px;
+  margin: 2rem auto;
+  padding: 0 2rem;
+`
 
 export default class Section1 extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Title>
-                    Why use React and D3 for <br />
-                    data visualization
-                </Title>
-                <Wrapper>
-                    {/*<img src={BarChart} alt='bc' />*/}
+  render() {
+    return (
+      <React.Fragment>
+        <Title>
+          Why use React and D3 for <br />
+          data visualization
+        </Title>
+        <Wrapper>
+          {/*<img src={BarChart} alt='bc' />*/}
 
-                    <P>
-                        I believe React combined with D3 is the best thing
-                        that's ever happened to data visualization on the web.
-                    </P>
-                    <P>
-                        React's approach to components makes your code more
-                        reusable, its advanced algorithms make it fast, and D3
-                        has the best tools for dataviz. Together they represent
-                        two of the most popular JavaScript libraries on the web.
-                    </P>
-                    <P>Watch some of my conference talks for a sneak peek 👇</P>
+          <P>
+            I believe React combined with D3 is the best thing that's ever
+            happened to data visualization on the web.
+          </P>
+          <P>
+            React's approach to components makes your code more reusable, its
+            advanced algorithms make it fast, and D3 has the best tools for
+            dataviz. Together they represent two of the most popular JavaScript
+            libraries on the web.
+          </P>
+          <P>Watch some of my conference talks for a sneak peek 👇</P>
 
-                    <Video videoId="UP1nCXG2t4M" />
-                    <P>
-                        The talk shows a proof of concept approach to making
-                        fancy animations with React and d3js - a Space Invaders
-                        game. I explain the basic approach, where I got the
-                        idea, and show off some code.
-                    </P>
-                    <Video videoId="47uMw-2mb4U" />
+          <Video videoId="UP1nCXG2t4M" />
+          <P>
+            The talk shows a proof of concept approach to making fancy
+            animations with React and d3js - a Space Invaders game. I explain
+            the basic approach, where I got the idea, and show off some code.
+          </P>
+          <Video videoId="47uMw-2mb4U" />
 
-                    <P>
-                        This talk is more hands on. I show how the animated
-                        alphabet, particle generator, and talk about the
-                        benefits of componentization when it comes to building
-                        modern data visualization.
-                    </P>
-                    <Video videoId="9JvIyz7uB2s" />
+          <P>
+            This talk is more hands on. I show how the animated alphabet,
+            particle generator, and talk about the benefits of componentization
+            when it comes to building modern data visualization.
+          </P>
+          <Video videoId="9JvIyz7uB2s" />
 
-                    <P>
-                        You don't have to watch the whole talk, it's all in
-                        React for Data Visualization.
-                    </P>
-                </Wrapper>
-            </React.Fragment>
-        );
-    }
+          <P>
+            You don't have to watch the whole talk, it's all in React for Data
+            Visualization.
+          </P>
+
+          <Video videoId="Bdeu-BFisJU" />
+          <P>
+            A Learn with Jason episode where in just an hour we built an
+            animated scatterplot from scratch and proved React-based D3
+            visualization works great with Gatsby. The dataviz rendered
+            statically at build time.
+          </P>
+        </Wrapper>
+      </React.Fragment>
+    )
+  }
 }
--- src/widgets/Section6.js
+++ src/widgets/Section6.js
@@ -1,89 +1,93 @@
-import React, { Component } from "react";
-import styled from "styled-components";
-import { Title } from "../styles";
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { Title } from '../styles'
 
-import Example from "../widgets/Examples";
-import examp1 from "../images/examp1.gif";
-import examp2 from "../images/examp2.gif";
-import examp3 from "../images/examp3.gif";
-import examp4 from "../images/examp4.gif";
-import examp5 from "../images/examp5.gif";
-import examp6 from "../images/examp6.gif";
-import examp7 from "../images/examp7.gif";
-import examp8 from "../images/examp8.gif";
+import Example from '../widgets/Examples'
+import examp1 from '../images/examp1.gif'
+import examp2 from '../images/examp2.gif'
+import examp3 from '../images/examp3.gif'
+import examp4 from '../images/examp4.gif'
+import examp5 from '../images/examp5.gif'
+import examp6 from '../images/examp6.gif'
+import examp7 from '../images/examp7.gif'
+import examp8 from '../images/examp8.gif'
+import examp0 from '../images/pietobar.gif'
 
 const Wrapper = styled.div`
-    max-width: 700px;
-    margin: 7rem auto;
-    padding: 0 2rem;
+  max-width: 700px;
+  margin: 7rem auto;
+  padding: 0 2rem;
+  h1 {
+    margin: 0;
+    text-align: center;
+  }
+  .dash {
+    height: 100px;
+  }
+  @media (max-width: 940px) {
     h1 {
-        margin: 0;
-        text-align: center;
+      font-size: 30px;
     }
     .dash {
-        height: 100px;
+      height: 50px;
     }
-    @media (max-width: 940px) {
-        h1 {
-            font-size: 30px;
-        }
-        .dash {
-            height: 50px;
-        }
-        .tree {
-            height: 200px;
-        }
+    .tree {
+      height: 200px;
     }
-`;
+  }
+`
 
 const Section6 = () => (
-    <>
-        <Title>React for Data Visualization builds on practical examples</Title>
-        <Wrapper>
-            <Example
-                image={examp1}
-                header="An interactive choropleth map"
-                copy="shows you how to work with topographical data, draw maps, and census
-            regions. We implement zooming and map exploration."
-            />
-            <Example
-                image={examp2}
-                header="An interactive histogram"
-                copy="teaches you some of D3's statistical functions, and the basics of
-            drawing charts. Drawing shapes, adding axes, parsing data ..."
-            />
-            <Example
-                image={examp3}
-                header="Interactive dashboard"
-                copy="Together they form an interactive dashboard where multiple charts share data, react to common controls, and act together to give users a full picture."
-            />
-            <Example
-                image={examp4}
-                header="A swipe transition"
-                copy="A swipe transition teaches you about building transition-based animation. Flip a boolean, see complex animation."
-            />
-            <Example
-                image={examp5}
-                header="An animated alphabet"
-                copy="An animated alphabet teaches you about creating enter/update/exit transitions. Animate elements coming in and out of your data visualization."
-            />
-            <Example
-                image={examp6}
-                header="A particle generator"
-                copy="A particle generator teaches you the basics of using Redux for state management, pushing your browser to the limits, and animating tens of thousands of elements."
-            />
-            <Example
-                image={examp7}
-                header="An interactive billiards game"
-                copy="An interactive billiards game teaches you about rendering graphical React components onto HTML5 canvas, making them interactive, and using MobX for state handling."
-            />
-            <Example
-                image={examp8}
-                header="A dancing pythagorean tree fractal"
-                copy="A dancing pythagorean tree fractal teaches you about component recursion. We use it to compare differences between React, Preact, and Inferno."
-            />
-        </Wrapper>
-    </>
-);
+  <>
+    <Title>React for Data Visualization builds on practical examples</Title>
+    <Wrapper>
+      <Example
+        image={examp0}
+        header="A piechart transitions to a barchart"
+        copy="Shows you how to use hybrid animation for data-driven effects. Even transitioning between different visualization types."
+      />
+      <Example
+        image={examp1}
+        header="An interactive choropleth map"
+        copy="Shows you how to work with topographical data, draw maps, and census regions. We implement zooming and map exploration."
+      />
+      <Example
+        image={examp2}
+        header="An interactive histogram"
+        copy="Teaches you some of D3's statistical functions, and the basics of drawing charts. Drawing shapes, adding axes, parsing data ..."
+      />
+      <Example
+        image={examp3}
+        header="Interactive dashboard"
+        copy="Together they form an interactive dashboard where multiple charts share data, react to common controls, and act together to give users a full picture."
+      />
+      <Example
+        image={examp4}
+        header="A swipe transition"
+        copy="A swipe transition teaches you about building transition-based animation. Flip a boolean, see complex animation."
+      />
+      <Example
+        image={examp5}
+        header="An animated alphabet"
+        copy="An animated alphabet teaches you about creating enter/update/exit transitions. Animate elements coming in and out of your data visualization."
+      />
+      <Example
+        image={examp6}
+        header="A particle generator"
+        copy="A particle generator teaches you the basics of using Redux for state management, pushing your browser to the limits, and animating tens of thousands of elements."
+      />
+      <Example
+        image={examp7}
+        header="An interactive billiards game"
+        copy="An interactive billiards game teaches you about rendering graphical React components onto HTML5 canvas, making them interactive, and using MobX for state handling."
+      />
+      <Example
+        image={examp8}
+        header="A dancing pythagorean tree fractal"
+        copy="A dancing pythagorean tree fractal teaches you about component recursion. We use it to compare differences between React, Preact, and Inferno."
+      />
+    </Wrapper>
+  </>
+)
 
-export default Section6;
+export default Section6
