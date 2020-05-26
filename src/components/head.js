456203bfffaedd6333fe5dfa6db803c5b5a9b9e4
escapemanuele <escapemanuele@gmail.com>
2020-05-26 13:10:51 +0200

    fix pages and tweet

--- src/components/head.js
+++ src/components/head.js
@@ -3,11 +3,9 @@ import { Helmet } from "react-helmet"
 import defaultThumb from "../images/metaimage.png"
 
 export default (props) => {
-  console.log("TTITII", props.title)
   const title = [props.title, "React for Data Visualization"]
     .filter(Boolean)
     .join(" | ")
-  console.log("TTITIAAAAAAAAI", title)
 
   const description =
     props.description ||
